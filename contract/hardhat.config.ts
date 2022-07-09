97813a044e5b76b1d807e709fe58bb2b4f18d2de
strandgeek <strandgeek@gmail.com>
2022-07-09 00:22:09 -0300

    feat: configure avalanche testnet

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -22,12 +22,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    fuji: {
+      url: process.env.FUJI_URL || "",
+      chainId: 43113,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const MintPlant = await ethers.getContractFactory("MintPlant");
+  const mintPlant = await MintPlant.deploy();
 
-  await greeter.deployed();
+  await mintPlant.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("MintPlant deployed to:", mintPlant.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
