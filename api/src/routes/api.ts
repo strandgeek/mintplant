64d4af5bd97fcb36c12927b0da81d4ff67a86ac2
strandgeek <strandgeek@gmail.com>
2022-07-13 01:02:57 -0300

    chore: remove old api

--- api/src/routes/api.ts
+++
@@ -1,9 +0,0 @@
-import express, { Express, Request, Response, Router } from 'express';
-
-export const apiRouter = Router()
-
-apiRouter.get('/config', (req: Request, res: Response) => {
-  res.json({
-    contractAddress: process.env.CONTRACT_ADDRESS,
-  })
-})
